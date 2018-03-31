9309883cc272c830e529739d9258a76427880607
LittlePanic <263476739@qq.com>
2018-03-31 10:27:23 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# FacialRecognition
+# FacialRecognition
+# FacialRecognition
---
+++ face_recognition_system.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def execShellCommand(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,executable="/bin/bash")
+
+    while True:  
+        buff = p.stdout.readline()  
+        if buff == '' and p.poll() != None:  
+            break
+    p.wait()
+ 
+ 
+def main():
+    cmd = 'source ~/.profile' +'&&' + 'workon cv'  + '&&' + 'python face_recognition.py'
+    execShellCommand(cmd)
+    
+if __name__ == "__main__":
+    while(True):
+        choose = raw_input('choose')  #
+        if choose == 'r':
+            main()
+        else:
+            print('thanks')
+            break
+        
+    
