96b8d7a49cf5a7ea93df9c2337e028fdc102b4f8
LittlePanic <263476739@qq.com>
2018-04-01 13:43:37 +0800

    update

--- test_tanchaung.py
+++ test_tanchaung.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-__author__ = '方昕成'
+import RPi.GPIO as GPIO
+import time
+import os
+
 
 
 def my_callback(channel):
-    print('This is a edge event callback function!')
-    print('Edge detected on channel %s'%channel)
-    print('This is run in a different thread to your main program')
+    print('sssssssssss')
+    os.system('python face_recognition_system.py')
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
 
-RPi.GPIO.setmode(GPIO.BOARD)
-RPi.GPIO.setup(11, RPi.GPIO.IN)
-GPIO.add_event_detect(11, GPIO.RISING, callback=my_callback)
-
+GPIO.add_event_detect(12, GPIO.RISING, callback=my_callback,bouncetime=40000)
+while(True):
+    pass
+GPIO.cleanup()
