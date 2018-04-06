961960a7da0a1e6e331adcda348f3aa392843326
LittlePanic <263476739@qq.com>
2018-04-06 08:58:23 +0800

    update

--- face_recognition.py
+++ face_recognition.py
@@ -30,24 +30,26 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video widht
 cam.set(4, 480) # set video height
 
+size = 0
+unsize = 0
 # Define min window size to be recognized as a face
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
+name = 'None'
 
 while True:
-    size = 0
 
     ret, img =cam.read()
     #img = cv2.flip(img, -1) # Flip vertically
-
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale( 
         gray,
         scaleFactor = 1.2,
         minNeighbors = 5,
         minSize = (int(minW), int(minH)),
        )
+    if str(faces) == '()':
+        unsize = unsize +1
 
     for(x,y,w,h) in faces:
 
@@ -57,24 +59,32 @@ while True:
 
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
-            id = names[id]
-            if confidence < 50:
+            if confidence < 60:
                 size = size + 1
+                name = names[id]
+                print(name)
+            id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
             id = "unknown"
+            unsize = unsize + 1
             confidence = "  {0}%".format(round(100 - confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     
-    cv2.imshow('camera',img) 
-    if size >= 10:
+    cv2.imshow('camera',img)
+    if unsize>300:
+        print('unknown')
+        break;
+    if size >10:
+        print('name is ' + name)
         break
+    
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
-
+    
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
